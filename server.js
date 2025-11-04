c9051f9479e65fbf4ee01df6f8cb33b9be4f7887
Vladislav Naraikin <randalv@yandex.ru>
2025-11-04 13:05:46 +0700

    files

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const app = express();
 app.use(express.json()); 
 
 if(process.env.NODE_ENV === 'production'){
-    //set static folder
+    app.get('/files/IQ_countries.csv', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'files', 'IQ_countries.csv'))
+    })
+    app.get('/files/shopping_habits.csv', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'files', 'shopping_habits.csv'))
+    })
+    app.get('/files/vgsale_1.csv', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'files', 'vgsale_1.csv'))
+    })
+
+    // Setting static folder
     app.use(express.static('client/dist'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
